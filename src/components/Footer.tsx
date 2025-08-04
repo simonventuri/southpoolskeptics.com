fab3e75f739e561186707fc0b82dcc9f4a504791
Simon Venturi <simonventuri@gmail.com>
2025-08-04 11:33:36 +0100

    Fixed email link in footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <footer>
-      <p>For further information, to sign up for notification of future events or to just say hello please email <a href="mailto:southpoolskeptis@gmail.com">southpoolskeptics@gmail.com</a></p>
+      <p>For further information, to sign up for notification of future events or to just say hello please email <a href="mailto:southpoolskeptics@gmail.com">southpoolskeptics@gmail.com</a></p>
       <p>&copy; {new Date().getFullYear()} South Pool Skeptics. All rights reserved.</p>
     </footer>
   );
